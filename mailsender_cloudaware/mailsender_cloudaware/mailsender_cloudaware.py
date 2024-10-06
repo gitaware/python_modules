01b1d43f87b8f9ae998d63f0b0d936c04f48e61a
CloudAware <153455885+cloudgitaware@users.noreply.github.com>
2024-10-06 13:48:03 +0200

    Update mailsender_cloudaware.py

--- mailsender_cloudaware/mailsender_cloudaware/mailsender_cloudaware.py
+++ mailsender_cloudaware/mailsender_cloudaware/mailsender_cloudaware.py
@@ -1,49 +1,88 @@
 #!/usr/bin/python3
 
-import email, mimetypes, smtplib
+import smtplib
+from email import encoders
+from email.utils import formataddr
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from mimetypes import guess_type
 import os
 
 class Mailsender:
-  def __init__( self, subject=None, body=None, sender={'name': None, 'address': None }, recipient={'name': None, 'address': None } , cc={'name': None, 'address': None }, bcc={'name': None, 'address': None }, mailserver={'hostname': 'localhost', 'port': 25 } ):
+  def __init__( self, subject=None, body=None, html_body=None, sender=None, recipient=None , cc=None, bcc=None, mailserver={'hostname': 'localhost', 'port': 25 } ):
     self.subject   = subject
-    self.body      = body
-    self.sender    = sender
-    self.recipient = recipient
-    self.CC        = cc
-    self.BCC       = bcc
+    self.txt_body  = body
+    self.html_body = html_body
+    self.sender    = None if sender is None    else formataddr((sender['name'], sender['address']))
+    self.recipient = None if recipient is None else formataddr((recipient['name'], recipient['address']))
+    self.CC        = None if cc is None        else formataddr((cc['name'], cc['address']))
+    self.BCC       = None if bcc is None       else formataddr((bcc['name'], bcc['address']))
     self.mailserver= mailserver
-    self.message   = email.message.EmailMessage()
-
-    if self.subject is not None:                           self.message.add_header("Subject", self.subject)
-    if self.sender    != {'name': None, 'address': None }: self.message.add_header("From", '"%s" <%s>'%(self.sender['name'], self.sender['address']) )
-    if self.recipient != {'name': None, 'address': None }: self.message.add_header("To",   '"%s" <%s>'%(self.recipient['name'], self.recipient['address']) )
-    if self.CC        != {'name': None, 'address': None }: self.message.add_header("Cc",   '"%s" <%s>'%(self.CC['name'], self.CC['address']) )
-    if self.BCC       != {'name': None, 'address': None }: self.message.add_header("Bcc", '"%s" <%s>'%(self.BCC['name'], self.BCC['address']) )
-    if self.body is not None:                              self.message.set_content(self.body)
+    self.message   = MIMEMultipart('mixed')
 
   def set_subject(self, subject):
     self.subject = subject
-    if self.subject is not None:                           self.message.add_header("Subject", self.subject)
 
   def set_body(self, body):
-    self.body = body
-    if self.body is not None:                              self.message.set_content(self.body)
+    self.set_txt_body(body)
+  
+  def set_txt_body(self, body):
+    self.txt_body = body
+
+  def set_html_body(self, body):
+    self.html_body = body
 
   def set_sender(self, sender):
-    self.sender = sender
-    if self.sender    != {'name': None, 'address': None }: self.message.add_header("From", '"%s" <%s>'%(self.sender['name'], self.sender['address']) )
+    self.sender = formataddr((sender['name'], sender['address']))
 
   def set_recipient(self, recipient):
-    self.recipient = recipient
-    if self.recipient != {'name': None, 'address': None }: self.message.add_header("To", '"%s" <%s>'%(self.recipient['name'], self.recipient['address']) )
+    self.recipient = formataddr((recipient['name'], recipient['address']))
+
+  def set_cc(self, cc):
+    self.CC = formataddr((cc['name'], cc['address']))
+
+  def set_bcc(self, bcc):
+    self.BCC = formataddr((bcc['name'], bcc['address']))
 
   def add_attachment(self, filepath):
+    mimetype, encoding = guess_type(filepath)
+    mimetype = mimetype.split('/', 1)
     with open(filepath, "rb") as attachment:
-      self.message.add_attachment(attachment.read(), maintype='text', subtype='html', filename=os.path.basename(filepath))
+      part = MIMEBase(mimetype[0], mimetype[1])
+      part.set_payload(attachment.read())
+
+    encoders.encode_base64(part)
+    part.add_header(
+    "Content-Disposition", 'attachment',
+    filename="%s"%(os.path.basename(filepath))
+    )
+    self.message.attach(part)
 
   def send(self):
+    self.message['Subject'] = self.subject
+    self.message['From']    = self.sender
+    self.message['To']      = self.recipient
+    if self.CC:
+      self.message['Cc']      = self.CC
+    if self.BCC:
+      self.message['Bcc']     = self.BCC
+    body = MIMEMultipart('alternative')
+    if self.txt_body is not None:
+      body.attach(MIMEText(self.txt_body, "plain"))
+    if self.html_body is not None:
+      body.attach(MIMEText(self.html_body, "html"))
+    self.message.attach(body)
+
     with smtplib.SMTP(self.mailserver['hostname'], self.mailserver['port']) as server:
       #server.login(sender_email, password)
-      server.send_message(msg=self.message)
+      server.send_message(self.message )
     #safety for mailings
-    self.message.clear()
+    self.subject   = None
+    self.txt_body  = None
+    self.html_body = None
+    self.sender    = None
+    self.recipient = None
+    self.CC        = None
+    self.BCC       = None
+    self.message   = MIMEMultipart('mixed')
